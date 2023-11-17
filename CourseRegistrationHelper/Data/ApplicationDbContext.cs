9422355fd582362251fb4812c4e840b7f6e4d3d7
Khalid Alanazi <khalid.oj@gmail.com>
2023-11-17 10:08:58 +0300

    Add project files.

---
+++ CourseRegistrationHelper/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CourseRegistrationHelper.Models;
+
+namespace CourseRegistrationHelper.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets for Universities, Colleges, etc. will be added here later.
+    }
+
+}
---
+++ CourseRegistrationHelper/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CourseRegistrationHelper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
