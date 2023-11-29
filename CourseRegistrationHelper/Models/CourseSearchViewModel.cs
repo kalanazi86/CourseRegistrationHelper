c6175590764c752713464752eed0b1af1834dd79
Khalid Alanazi <khalid.oj@gmail.com>
2023-11-29 13:39:19 +0300

    last update before implementing suggested schedules page

--- CourseRegistrationHelper/Models/CourseSearchViewModel.cs
+++ CourseRegistrationHelper/Models/CourseSearchViewModel.cs
@@ -3,21 +3,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CourseRegistrationHelper.Models
 {
-    //public class CourseSearchViewModel
-    //{
-    //    public List<University> Universities { get; set; } // For dropdown
-    //    public List<College> Colleges { get; set; }       // For dropdown
-    //    public List<Department> Departments { get; set; } // For dropdown
-    //    public List<Course> Courses { get; set; }
-    //    public List<SectionViewModel> Sections { get; set; } //List for sections to display
-
-    //    // Add properties for selected values if you need to retain dropdown selections
-    //    public int SelectedUniversityId { get; set; }
-    //    public int SelectedCollegeId { get; set; }
-    //    public int SelectedDepartmentId { get; set; }
-    //    public int SelectedCourseId { get; set; }
-
-    //}
+    
     public class CourseSearchViewModel
     {
         // Change these properties to use SelectListItems for the dropdowns
